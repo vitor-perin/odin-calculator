2f14c2d014f964e51308b65cfb55866c0e11bf7c
vitor-perin <vitor-perin@outlook.com>
2022-09-30 21:23:55 -0300

    Sub, Add, Div, Mult, buttons working

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const currentOperation = document.querySelector('.current-operation');
 const previousOperation = document.querySelector('.previous-operation');
 const buttons = document.querySelectorAll('button');
 
+let digit = null;
 
 //Event listener
 buttons.forEach((btn) => {
@@ -11,35 +12,72 @@ buttons.forEach((btn) => {
 
         if(+value >= 0 || value == '.'){
             if(currentOperation.textContent.includes('.') && value == '.') return;
-            appendValue(value);
+            
+            digit = value
+            appendValue();
         }else {
-            operation(value);
+            operationCheck(value);
         }
     })
 })
 
 //Append the value to the screen
-function appendValue(value) {
-    currentOperation.textContent += value;
-}
-
-//Check type of operation
-function operation(value) {
-    switch (value) {
-        case ('C'):
-            clearScreen();
-            break;
-        
-        case ('CE'):
-            
-        default:
-            break;
+function appendValue(operationValue = null, operator = null, current = null, previous = null) {
+    if(operationValue === null){
+        currentOperation.textContent += digit;
+    } else{
+        if(previous === 0){
+            operationValue = current;
+        }
+        previousOperation.textContent = `${operationValue} ${operator}`;
+            currentOperation.textContent = '';
     }
 }
 
-//Clear Screen
+//Check type of operation
+function operationCheck(operator) {
+    let operationValue;
+    let previous = +previousOperation.textContent.split(' ')[0];
+    let current = +currentOperation.textContent;
 
-function clearScreen() {
+    switch (operator) {
+        case 'C':
+            clear();
+            break;
+        case 'CE':
+            clearCurrent();
+            break;
+        case '/':
+            operationValue = previous / current;
+            appendValue(operationValue, operator, current, previous);
+            break;
+        case '*':
+            operationValue = previous * current;
+            appendValue(operationValue, operator, current, previous);
+            break;
+        case '-':
+            operationValue = previous - current;
+            appendValue(operationValue, operator, current, previous);
+            break;
+        case '+':
+            operationValue = previous + current;
+            appendValue(operationValue, operator, current, previous);
+            break;
+        case '=':
+            
+            break;
+        default:
+            return;
+    }
+}
+
+//Clear
+
+function clear() {
     currentOperation.textContent = '';
     previousOperation.textContent = '';
+}
+
+function clearCurrent() {
+    currentOperation.textContent = '';
 }
